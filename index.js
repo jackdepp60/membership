48468098befd65471f7c9a2565cb146ce99afeef
Jack Depp <grayem69@gmail.com>
2022-08-30 19:27:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var test = document.querySelector(".log-in-btn");
 
 test.addEventListener("click", function goToMain(){
     // alert("test")
-    window.location.href = "tabs.html";
+    window.location.href = "main.html";
 })
 
