d2e5305c5e65930fd325ecedd9603a7f3f5fc502
Jack Depp <grayem69@gmail.com>
2022-08-30 22:12:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var test = document.querySelector(".log-in-btn");
 
 test.addEventListener("click", function goToMain(){
-    // alert("test")
+    alert("test");
     window.location.href = "https://jackdepp60.github.io/membership/main.html";
 })
 
